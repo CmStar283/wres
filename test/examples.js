9c75a35bccc496f50fcea1aaa4e2e0eb4c642d53
Cm_Star <imagineer023@me.com>
2015-10-23 16:05:35 -0400

    Add test scenarios

---
+++ test/examples.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+var wres = require('..');
+
+describe('Examples', function() {
+  fs.readdirSync(path.resolve(__dirname, '../examples')).forEach(function (e) {
+    if (/\.(yml|json)/.test(e)) {
+      it(path.basename(e, path.extname(e)) + 'renders without error', function() {
+        assert.doesNotThrow(function() {
+          wres.render(
+            null,
+            null,
+            fs.readFileSync(path.resolve(__dirname, '../examples/' + e), 'utf8'),
+            null
+          );
+        });
+      });
+    }
+  });
+});
