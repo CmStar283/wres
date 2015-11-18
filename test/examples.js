6fc5ddd1a21fe86235b03a30cc11c3f71c0d187c
Cm_Star <imagineer023@me.com>
2015-11-18 17:28:55 -0500

    Improve error handling and test scenarios

--- test/examples.js
+++ test/examples.js
@@ -5,19 +5,11 @@ var path = require('path');
 var assert = require('assert');
 var wres = require('..');
 
-describe('Examples', function() {
-  fs.readdirSync(path.resolve(__dirname, '../examples')).forEach(function (e) {
-    if (/\.(yml|json)/.test(e)) {
-      it(path.basename(e, path.extname(e)) + ' renders without error', function() {
-        assert.doesNotThrow(function() {
-          wres.render(
-            null,
-            null,
-            fs.readFileSync(path.resolve(__dirname, '../examples/' + e), 'utf8'),
-            null
-          );
-        });
-      });
-    }
+describe('Error Handling', function() {
+  it('null resume throws error', function() {
+    assert.throws(function() {
+      wres.render(null, null, null, null);
+      }, Error
+    );
   });
 });
