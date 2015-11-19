2d1f2171da1817d0ed1fadaee5044ce92f9abe73
Cm_Star <imagineer023@me.com>
2015-11-18 20:21:56 -0500

    Simplify test code for errors

--- test/errors.js
+++ test/errors.js
@@ -1,15 +1,17 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 var assert = require('assert');
 var wres = require('..');
 
 describe('Error Handling', function() {
   it('null resume throws error', function() {
     assert.throws(function() {
-      wres.render(null, null, null, null);
-      }, Error
-    );
+      wres.render();
+    }, Error);
+  });
+  it('invalid resume throws error', function() {
+    assert.throws(function() {
+      wres.render('*');
+    }, Error);
   });
 });
