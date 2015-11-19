8ce5feba2c73d020bf6db43be9fbfeb8aa210603
Cm_Star <imagineer023@me.com>
2015-11-18 19:47:11 -0500

    Rename test scripts to appropriate names

--- test/errors.js
+++ test/errors.js
@@ -4,34 +4,12 @@ var fs = require('fs');
 var path = require('path');
 var assert = require('assert');
 var wres = require('..');
-var yaml = require('js-yaml');
 
-describe('Examples', function() {
-  fs.readdirSync(path.resolve(__dirname, '../examples')).forEach(function (e) {
-    // Load each example into a YAML and JSON string, if applicable
-    var r_yaml, r_json;
-    if(/\.yml/.test(e)) {
-      r_yaml = fs.readFileSync(path.resolve(__dirname, '../examples/' + e), 'utf8');
-      r_json = JSON.stringify(yaml.safeLoad(r_yaml));
-    } else if (/\.json/.test(e)) {
-      r_json = fs.readFileSync(path.resolve(__dirname, '../examples/' + e), 'utf8');
-      r_yaml = yaml.safeDump(r_json);
-    } else {
-      return; // Not a YAML or JSON string, so skip
-    }
-
-    var example = path.basename(e, path.extname(e)) // Determine name of the exmaple
-
-    // Test wres render with YAML and JSON formats of each example
-    it(example + ' (yaml) renders without error', function() {
-      assert.doesNotThrow(function() {
-        wres.render(r_yaml, null, null, null);
-      });
-    });
-    it(path.basename(e, path.extname(e)) + ' (json) renders without error', function() {
-      assert.doesNotThrow(function() {
-        wres.render(r_json, null, null, null);
-      });
-    });
+describe('Error Handling', function() {
+  it('null resume throws error', function() {
+    assert.throws(function() {
+      wres.render(null, null, null, null);
+      }, Error
+    );
   });
 });
